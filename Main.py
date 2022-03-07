a761412f18f439dac630577f6bea430c313906d0
tmarchia <74574315+tmarchia@users.noreply.github.com>
2022-03-06 19:43:27 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,39 @@
+"""
+Main file that will run all of the user stories
+"""
+from GedcomClass import Gedcom
+from US01 import US01_DateBeforeCurrentDate
+from US05 import US05_MarriageBeforeDeath
+
+def Main():
+    #set up the class based on an input file
+    ged : Gedcom = Gedcom('/Users/tylermarchiano/Documents/Stevens/SSW555/Project/User Stories/Input_TylerMarchiano.ged')
+    individuals = ged.people.keys()
+    families = ged.families.keys()
+    
+    #Run US01
+    #Need to check all dates so need to check individuals and families
+    for person in individuals:
+        key = person
+        value = ged.people[person]
+        dateTags = ['BIRT', 'DEAT', 'DIV','MARR']
+        US01_DateBeforeCurrentDate(key, value)
+    
+    for family in families:
+        key = family
+        value = ged.families[family]
+        dateTags = ['BIRT', 'DEAT', 'DIV','MARR']
+        US01_DateBeforeCurrentDate(key, value)
+    
+    #Run US05
+    #Only Marriage date matters so can just check families
+    for family in families:
+        key = family
+        value = ged.families[family]
+        husb_id = value['HUSB']
+        wife_id = value['WIFE']
+        husb_info = ged.people[husb_id]
+        wife_info = ged.people[wife_id]
+        US05_MarriageBeforeDeath(key, value, husb_info, wife_info)
+
+Main()
